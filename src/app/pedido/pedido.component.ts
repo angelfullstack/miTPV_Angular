a92c0cf099e0656a4cad658babf1cc35f6ee457b
angelfullstack <angelfullstack@gmail.com>
2020-02-09 10:51:54 +0100

    Avanzando en CSS

--- src/app/pedido/pedido.component.ts
+++ src/app/pedido/pedido.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, DoCheck } from '@angular/core';
 import { Producto } from '../models/producto';
+import { runInThisContext } from 'vm';
 
 
 @Component({
@@ -11,15 +12,30 @@ export class PedidoComponent implements OnInit {
   @Input() listaPedido: Producto[];
   @Input() precioTotal: number;
   totalPedido: Producto[];
+  listaVacia: string[];
+  longitudPedido: number;
   /* He intentado tipar algo así como EventEmitter<Producto,string> pero no me deja. Tampoco EventEmitter[<Producto>,string] */
   @Output() modificadorCantidad: EventEmitter<any>;
   constructor() {
     this.listaPedido = [];
     this.modificadorCantidad = new EventEmitter();
+    this.listaVacia = [];
+    this.listaVacia[10] = '';
+    this.longitudPedido = 0;
+
   }
 
   ngOnInit() {
-    console.log(this.listaPedido)
+
+  }
+
+  /* Cuando hay cambios en el componente se chequea...  */
+  ngDoCheck() {
+    /* Quiero que cada vez que aparezca un producto y la lista supere el height, la lista haga scroll al último elemento */
+    /* this.pedido[0].scrollTop = this.pedido[0].scrollHeight; */
+
+
+
   }
 
   aumentarUnidad(producto) {
@@ -29,6 +45,5 @@ export class PedidoComponent implements OnInit {
     this.modificadorCantidad.emit([producto, 'quitar']);
   }
 
-
-
 }
+
